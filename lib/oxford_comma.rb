af062c7ff0c1aab645ce24b1c26b44a039dc57f3
Oscar Alvarez <gavinalv6@gmail.com>
2020-08-02 18:21:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,13 @@
 def oxford_comma(array)
-  array.join
+  def oxford_comma(array)
+  if array.nil? || array.length <= 1
+    array.join
+  elsif array.length ==2
+    array.join(' and ')
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(', ')
+  end
 end
 
 
